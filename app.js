917bffae7fbdf6b60dcff7e41616995c2593c9e5
tomhon <tomhon@hotmail.com>
2016-11-16 13:01:13 -0800

    Updated local debug port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 var server = restify.createServer(); 
-server.listen(process.env.port || 80, function () {
+server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
 
